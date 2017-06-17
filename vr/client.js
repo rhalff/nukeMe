47d2e928f5bf3c9305cf808d8ca3e4c5b1d134d2
Rob Halff <rob.halff@gmail.com>
2017-06-17 17:17:36 +0100

    lint

--- vr/client.js
+++ vr/client.js
@@ -1,18 +1,17 @@
-import {VRInstance} from 'react-vr-web';
+import { VRInstance } from "react-vr-web";
 
 // Not used worker -> main
 function onVRMessage(event) {
   // console.log('Or arrives here?', event);
   switch (event.data.type) {
-
   }
-}
+e
 
 function init(bundle, parent, options) {
-  const vr = new VRInstance(bundle, 'Aurealme', parent, {
+  const vr = new VRInstance(bundle, "Aurealme", parent, {
     // Add custom options here
     ...options,
-    nativeModules: [ ]
+    nativeModules: []
   });
   vr.render = function() {
     // Any custom behavior you want to perform on each frame goes here
@@ -20,27 +19,27 @@ function init(bundle, parent, options) {
   // Begin the animation loop
   vr.start();
 
-  vr.rootView.context.worker.addEventListener('message', onVRMessage);
+  vr.rootView.context.worker.addEventListener("message", onVRMessage);
 
   sensors.enable(sensors.ACCELEROMETER);
 
   setInterval(() => {
-    sensors.getState(sensors.ACCELEROMETER, (payload) => {
+    sensors.getState(sensors.ACCELEROMETER, payload => {
       const { rootView: { context: { worker } } } = vr;
 
-      console.log('Sending but never arrives?');
+      console.log("Sending but never arrives?");
 
       // this is crazy, same, but doesn't work
       worker.postMessage({
-        type: 'accel',
+        type: "accel",
         payload: payload
       });
     });
   }, 2000);
 
-  console.log('ReactVR initialized');
+  console.log("ReactVR initialized");
 
   return vr;
 }
 
-window.ReactVR = {init};
+window.ReactVR = { init };
